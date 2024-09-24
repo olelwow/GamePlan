722e1d1d9943a861b895bf75e3a925e5e21abbd7
Jonas Öhrn <theeagle87@hotmail.com>
2024-09-24 10:37:15 +0200

    Added component week with every weekday inside.

---
+++ GamePlan.Web/src/Components/Week.jsx
@@ -0,0 +1,17 @@
+const Week = () => {
+
+    return(
+        <>
+        <div>Monday</div>
+        <div>Tuesday</div>
+        <div>Wednesday</div>
+        <div>Thursday</div>
+        <div>Friday</div>
+        <div>Saturday</div>
+        <div>Sunday</div>
+        </>
+    );
+
+}
+
+export default Week;
