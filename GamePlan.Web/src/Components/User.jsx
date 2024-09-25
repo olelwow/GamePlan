7fc662aa94f42c0e0591e5e2e2dd3f0e31761f07
nikospr90 <nick_prevenios@hotmail.com>
2024-09-25 14:53:36 +0200

    must save the file first

--- GamePlan.Web/src/Components/User.jsx
+++ GamePlan.Web/src/Components/User.jsx
@@ -3,20 +3,19 @@ import { useState } from "react";
 const host = "https://localhost:7136/";
 let data = {};
 const User = () => {
-    const [user, setUser] = useState({})
 
+    const [user, setUser] = useState({})
 
     async function handleClick() {
         const res = await fetch (`${host}api/users/${1}`)
         data = await res.json()
         setUser(prevUser => prevUser = data)
-        // console.log(data);
     }
 
     return <div>
             <button onClick={handleClick}>Get User</button>
             <p>Username: {user.userName}</p>
-            <p>id: {user.id}</p>
+
             <p>Xp: {user.xp}</p>
             </div>;
     };
