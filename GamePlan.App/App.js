7392b02a0247770c0cb7ef5115c3204ed55eac56
olelwow <ristenstrand66@gmail.com>
2024-09-30 11:13:18 +0200

    Moved components from web to app.

--- GamePlan.App/App.js
+++ GamePlan.App/App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Navbar from './components/Navbar';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Navbar from "./components/Navbar";
+import Week from "./components/Week";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Navbar />
-      <Text>Open up App.js to start working on your app! Hej Jonas!</Text>
+      <Week />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- GamePlan.App/components/UserMenu.js
+++ GamePlan.App/components/UserMenu.js
@@ -1,60 +1,61 @@
-import { FlatList, SectionList } from "react-native-web";
-import { Text, Image, View } from "react-native";
+import { Text, Image, View, FlatList, SectionList } from "react-native";
 import { useState } from "react";
 import fishSteer from "../assets/images/fishSteer.gif";
 import levelIcon from "../assets/images/levelIcon.png";
 import experienceIcon from "../assets/images/experienceIcon.png";
 
-
-
 const UserMenu = (props) => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const menuOptions = ["User Settings", "Log out", "Whatever"];
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const menuOptions = ["User Settings", "Log out", "Whatever"];
 
-    const toggleDropdown = () => {
-        setIsDropdownOpen((prevState) => !prevState);
-    };
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prevState) => !prevState);
+  };
 
-    return (
-        <View className="userContainer">
-            {isDropdownOpen ? (
-                <View className="userDropdown">
-                    <View className="userInfo">
-                        <Image
-                            source={fishSteer}
-                            alt="user icon"
-                            className="userImg"
-                            onClick={toggleDropdown}
-                        />
-                        <Text>{props.userName}</Text>
-                        <View className="userDetails">
-                            <View className="userLevel">
-                                <Image className="levelIcon" source={levelIcon} alt="level" />
-                                <Text className="userH3">{props.level}</Text>
-                            </View>
-                            <View className="userXp">
-                                <Image className="xpIcon" source={experienceIcon} alt="experience" />
-                                <Text className="userH3">{props.xp}</Text>
-                            </View>
-                        </View>
-                    </View>
-                    <SectionList className="dropdownList">
-                        {menuOptions.map((item) => (
-                            <View className="dropdownItem" key={item}>
-                                <Text>{item}</Text>
-                            </View>
-                        ))}
-                    </SectionList>
-                </View>
-            ) : (
+  return (
+    <View className="userContainer">
+      {isDropdownOpen ? (
+        <View className="userDropdown">
+          <View className="userInfo">
+            <Image
+              source={fishSteer}
+              alt="user icon"
+              className="userImg"
+              // onClick={toggleDropdown}
+            />
+            <Text>{props.userName}</Text>
+            <View className="userDetails">
+              <View className="userLevel">
+                <Image className="levelIcon" source={levelIcon} alt="level" />
+                <Text className="userH3">{props.level}</Text>
+              </View>
+              <View className="userXp">
                 <Image
-                    source={fishSteer}
-                    alt="user icon"
-                    className="userImg"
-                    onClick={toggleDropdown}
+                  className="xpIcon"
+                  source={experienceIcon}
+                  alt="experience"
                 />
-            )}
+                <Text className="userH3">{props.xp}</Text>
+              </View>
+            </View>
+          </View>
+          <SectionList className="dropdownList">
+            {menuOptions.map((item) => (
+              <View className="dropdownItem" key={item}>
+                <Text>{item}</Text>
+              </View>
+            ))}
+          </SectionList>
         </View>
-    );
+      ) : (
+        <Image
+          source={fishSteer}
+          alt="user icon"
+          className="userImg"
+          // onClick={toggleDropdown}
+        />
+      )}
+    </View>
+  );
 };
 export default UserMenu;
