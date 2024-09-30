fa36fe39cda8d60ad203db76614ed1f3ef42bc5c
Olle Ristenstrand <olelwow@hotmail.com>
2024-09-30 18:40:34 +0200

    Removed unused code.

--- GamePlan.App/components/Week.js
+++ GamePlan.App/components/Week.js
@@ -6,15 +6,9 @@ import { useContext } from "react";
 const Week = () => {
   const { weekDays } = useContext(WeekContext);
 
-  const dates = [];
-  for (let i = 0; i < 7; i++) {
-    const date = new Date(weekDays[i]);
-    dates.push(date);
-  }
-
   return (
     <View className="weekday">
-      {dates.map((day, index) => (
+      {weekDays.map((day, index) => (
         <Day key={index} day={day} />
       ))}
     </View>
