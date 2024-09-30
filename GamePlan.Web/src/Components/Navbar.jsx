a8df9584d94459e0bda76658966f8f2e97d8ea46
Johodah <johnny.dahle@hotmail.com>
2024-09-30 13:02:27 +0200

    Added week number to navbar and buttons to switch between.

--- GamePlan.Web/src/Components/Navbar.jsx
+++ GamePlan.Web/src/Components/Navbar.jsx
@@ -6,11 +6,28 @@ import NavbarBackground from "../assets/images/Background_main.png";
 const Navbar = () => {
   const [goalXp, setGoalXp] = useState(200);
   const [user, setUser] = useState({});
+  const [weekNumber, setWeekNumber] = useState(null);
 
   const viewMonth = () => {
     const date = new Date();
     const month = date.toLocaleString("default", { month: "long" });
     return month;
+  };
+
+  const getWeekNumber = (d) => {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+    return [d.getFullYear(), weekNo];
+  }
+
+  const handleNextWeek = () => {
+    setWeekNumber(prevWeek => prevWeek + 1);
+  };
+
+  const handlePrevWeek = () => {
+    setWeekNumber(prevWeek => prevWeek - 1);
   };
 
   // Get user object from api
@@ -23,6 +40,9 @@ const Navbar = () => {
 
   useEffect(() => {
     getUser();
+    const date = new Date();
+    const [year, week] = getWeekNumber(date);
+    setWeekNumber(week);
     console.log("using effect");
   }, []);
 
@@ -33,6 +53,9 @@ const Navbar = () => {
       </div>
       <div className="navbarCenter">
         <h2 className="viewMonth">{viewMonth()}</h2>
+        <h2 className="viewWeek">{weekNumber}</h2>
+        <button onClick={handlePrevWeek}>Prev</button>
+        <button onClick={handleNextWeek}>Next</button>
         <div className="goal" style={xpBar(user.xp / 2)}>
           <p>
             Weekly goal: {user.xp}/{goalXp}
