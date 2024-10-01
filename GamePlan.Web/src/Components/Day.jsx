839bba14c5027f18ade67332ca4d3512faf20639
nikospr90 <nick_prevenios@hotmail.com>
2024-10-01 17:01:44 +0200

    Aktivitet renderas på rutan nu

--- GamePlan.Web/src/Components/Day.jsx
+++ GamePlan.Web/src/Components/Day.jsx
@@ -3,10 +3,20 @@ import ArrowDown from "../assets/images/arrowDown.png";
 import ArrowUp from "../assets/images/arrowUp.png";
 
 const Day = (props) => {
+  let objectifiedDay = new Date(props.day);
+  let convertedDate = objectifiedDay.toISOString().split("T")[0];
+
   const [expandDay, setExpandDay] = useState(false);
-  const [activities, setActivities] = useState([{}])
+  const [activities, setActivities] = useState([]);
+  const [user, setUser] = useState([]);
+  const [userActivities, setUserActivities] = useState([]);
   const [isClicked, setIsClicked] = useState(false);
-  const [isSelected, setIsSelected] = useState(false);
+  const [selectedActivityId, setSelectedActivityId] = useState("");
+  const [date, setDate] = useState(props.day);
+
+  // states for styling
+  const [onClick, setOnClick] = useState(false);
+  const [borderRadius, setBorderRadius] = useState("5px");
 
   const TodaysDate = () => {
     const date = new Date();
@@ -16,7 +26,6 @@ const Day = (props) => {
     });
     return todaysDate;
   };
-
   //useeffect to fetch activities and display them when the
   //day is expanded and wanting to add activity to that day, show as a list or something.
   useEffect(() => {
@@ -27,6 +36,7 @@ const Day = (props) => {
         const data = await response.json();
         if (expandedDay) {
           setActivities(data);
+          getUser();
         }
       };
       fetchActivities();
@@ -36,109 +46,144 @@ const Day = (props) => {
     };
   }, [expandDay]);
 
-  const dayWednesday = props.day.getDay() === 3;
-  const daySaturday = props.day.getDay() === 6;
+  // get the current logged in user
+  async function getUser() {
+    const activityList = await fetch(
+      "https://localhost:7136/api/users/3/activities"
+    );
+    const person = await fetch("https://localhost:7136/api/users/3");
+    const data = await activityList.json();
+    const userData = await person.json();
+    setUserActivities(data);
+    setUser(userData);
+  }
 
+  // handles the click on the add activity button
   const AddActivity = async () => {
-    setIsClicked(prevState => !prevState);
+    setIsClicked((prevState) => !prevState);
   };
-    
-  const SaveActivity = async (obj) => {
+
+  //function that saves an activity on the user to the database
+  async function SaveActivity(activity) {
     const res = await fetch("https://localhost:7136/api/activities/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(obj),
+      body: JSON.stringify(activity),
     });
-
-    if (!res.ok){
+    if (!res.ok) {
       console.log("Error when saving activity");
+    } else {
+      console.log("Activity saved");
     }
-    console.log("Activity saved");
   }
 
-  function handleChange(event){
-    setIsSelected(event.target.value);
+  //function that get the selected option from the select list
+  function handleChange(event) {
+    setSelectedActivityId(event.target.value);
   }
-    
 
+  //function that handles the submit of the
+  //form and saves the activity to the user
+  function handleSubmit(event) {
+    event.preventDefault();
+    getUser();
+    //iterate the activities and find the selected activity
+    const activityArray = activities;
+    let selectedActivity;
+    for (var activity in activityArray) {
+      if (activityArray[activity].id === parseInt(selectedActivityId)) {
+        selectedActivity = activityArray[activity];
+      }
+    }
+    // send the activity as DTO model to the save function
+    SaveActivity({
+      name: selectedActivity.name,
+      xp: selectedActivity.xp,
+      date: date,
+      userId: user.id,
+    });
+  }
 
+  //function that removes an activity from the user
   const RemoveActivity = () => {
     console.log(`Ta bort aktivitet ${TodaysDate()}`);
+  };
+
+  //handles the states of these elements to change the styling
+  function handleClick() {
+    setOnClick((prevState) => !prevState);
+    setBorderRadius(onClick ? "5px" : "5px 5px 0px 0px");
   }
-
-const [onClick, setOnClick] = useState(false);
-const [borderRadius, setBorderRadius] = useState('5px');
-
-function handleClick() {
-  setOnClick(prevState => !prevState);
-  setBorderRadius(onClick ? '5px' : '5px 5px 0px 0px');
-}
 
   return (
     <>
-      <button 
-      style={
-         {borderRadius: borderRadius} 
-        }
-      className="day" 
-      onClick={
-        function () 
-        {
-          setExpandDay(!expandDay),
-          handleClick()
-        }}>
+      <button
+        style={{ borderRadius: borderRadius }}
+        className="day"
+        // name={props.day}
+        onClick={function () {
+          setExpandDay(!expandDay), handleClick();
+        }}
+      >
         {TodaysDate()}
         <p>3st</p>
-        <img 
-          src={onClick ?  ArrowUp : ArrowDown} 
-          alt="arrow" 
-          className="arrow" 
+        <img
+          src={onClick ? ArrowUp : ArrowDown}
+          alt="arrow"
+          className="arrow"
         />
       </button>
-      
-      {expandDay && (
-        <div 
-        id="dayContent"
-        
-        >
-          <p className="activitySummary">Aktivitet för {TodaysDate()}</p>
-          {dayWednesday &&
-            activities.find((activity) => activity.name === "Study") && (
-              <p>Idag är det Study</p>
-            )}
-          {daySaturday &&
-            activities.find((activity) => activity.name === "Gym") && (
-              <p>Idag är det Gym</p>
-            )}
-            <div className="buttonContainer">
-              <button
-                className="addButton" 
-                onClick= {AddActivity}>
-                  + 
-              </button>
-                {isClicked &&
-                <div>                
-                  <select onChange={handleChange}>
-                    {activities.map((activity) => (
-                      <option 
-                      key={activity.id}
-                      value={activity.name}
-                      >
-                        {activity.name}
-                        </option>
-                    ))}
-                  </select>
-                  <button onClick={() => SaveActivity(isSelected)}>Save</button>
-                </div>
-                }
 
-              {activities && <button
-                className="removeButton" 
-              onClick=
-              {RemoveActivity}> - 
-              </button>}
+      {expandDay && (
+        <div id="dayContent">
+          <p className="activitySummary">Aktivitet för {TodaysDate()}</p>
+
+          {userActivities && userActivities?.length > 0 ? (
+            userActivities?.map((activity) => {
+              const activityDate = activity.date.split("T")[0];
+              return activityDate === convertedDate ? (
+                <p className="activity" key={activity.id}>
+                  Idag är det ❤️{activity.name}❤️
+                </p>
+              ) : null;
+            })
+          ) : (
+            <p>No activities found</p>
+          )}
+
+          <div className="buttonContainer">
+            <button className="addButton" onClick={AddActivity}>
+              +
+            </button>
+            {isClicked && (
+              <form onSubmit={handleSubmit}>
+                <select onChange={handleChange}>
+                  <option value="" disabled>
+                    Select an activity
+                  </option>
+                  {activities.map((activity) => (
+                    <option
+                      key={activity.id}
+                      value={activity.id}
+                      name={activity}
+                    >
+                      {activity.name}
+                    </option>
+                  ))}
+                </select>
+                <button type="submit">Save</button>
+                {/* () => SaveActivity(isSelected) */}
+              </form>
+            )}
+
+            {activities && (
+              <button className="removeButton" onClick={RemoveActivity}>
+                {" "}
+                -
+              </button>
+            )}
           </div>
         </div>
       )}
