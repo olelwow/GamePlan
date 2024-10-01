2b3eb3f1f5e2dc53131ed00456088a03396cb68f
Rasmus <rbroman@live.se>
2024-10-01 14:18:25 +0200

    Edited week and app

--- GamePlan.Web/src/Components/Week.jsx
+++ GamePlan.Web/src/Components/Week.jsx
@@ -2,14 +2,14 @@ import Day from "./Day";
 import { useContext } from "react";
 import { WeekContext } from "./WeekContext";
 
-const Week = () => {
+const Week = (props) => {
     const { weekDays } = useContext(WeekContext);
 
   return (
     <>
     <div className="weekday">
       {weekDays.map((day, index) => (
-        <Day key={index} day={day} user={user}/>
+        <Day key={index} day={day} user={props.user}/>
       ))}
       </div>
     </>
